e1fadb027ee4d662f0e97b639783ab3e377286ea
Jeffrey Lien <jeffreylienxd@gmail.com>
2021-06-17 17:04:12 -0700

    feat: created exercise and user models, and set routers for the server

--- mern-exercise-tracker-website/backend/server.js
+++ mern-exercise-tracker-website/backend/server.js
@@ -19,6 +19,12 @@ connection.once('open', () => {
   console.log('MongoDB database connection established successfully')
 })
 
+const exerciseRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
 })
