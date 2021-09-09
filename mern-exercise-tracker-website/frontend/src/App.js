48dd527ceefeb1696dea1425838aeaf1ed6e9f6f
pjsrcool <justinpham79@outlook.com>
2021-09-09 13:31:41 -0700

    chore: ran npm prettier on frontend/src folder

--- mern-exercise-tracker-website/frontend/src/App.js
+++ mern-exercise-tracker-website/frontend/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <br/>
+      <br />
       <Route path="/" exact component={ExercisesList} />
       <Route path="/edit/:id" component={EditExercise} />
       <Route path="/create" component={CreateExercise} />
