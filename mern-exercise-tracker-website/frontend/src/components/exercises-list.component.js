945293cad5facdf649fddd2b72af5e754c07ce7f
pjsrcool <justinpham79@outlook.com>
2021-09-17 18:55:15 -0700

    bugfix: fixed exercises list not rendering

--- mern-exercise-tracker-website/frontend/src/components/exercises-list.component.js
+++ mern-exercise-tracker-website/frontend/src/components/exercises-list.component.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-const Exercise = (props) => {
+const Exercise = (props) => (
   <tr>
     <td>{props.exercise.username}</td>
     <td>{props.exercise.description}</td>
@@ -19,8 +19,8 @@ const Exercise = (props) => {
         delete
       </a>
     </td>
-  </tr>;
-};
+  </tr>
+);
 
 export default class ExercisesList extends Component {
   constructor(props) {
